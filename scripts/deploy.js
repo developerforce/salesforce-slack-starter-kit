dbd58283485982da470f67c0c7394b599280fedc
Alba Rivas <arivas@salesforce.com>
2021-11-25 14:43:19 +0100

    Fix issues with username-password flow

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -61,7 +61,7 @@ async function getuserinput() {
   log("");
   log(chalk.bold("*** Please provide the following information: "));
   const response = await userInputPrompt();
-  log("response: " + response);
+  log("response: " + JSON.stringify(response));
   sh.env.SF_DEV_HUB = response.devhub;
   sh.env.SF_SCRATCH_ORG = response.scratchorg;
   sh.env.SF_OAUTH_FLOW = response["oauth-flow"];
