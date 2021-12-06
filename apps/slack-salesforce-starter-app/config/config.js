17cfc56811fa32cba962db418bdb8eae02fca4bb
Mohith Shrivastava <mohith.shrivastava@salesforce.com>
2021-12-05 22:52:28 -0500

    feat: switch off socket mode

--- apps/slack-salesforce-starter-app/config/config.js
+++ apps/slack-salesforce-starter-app/config/config.js
@@ -17,8 +17,8 @@ const slack = {
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     port: process.env.PORT || 3000,
-    socketMode: true,
-    appToken: process.env.SLACK_APP_TOKEN
+    //socketMode: true,
+    //appToken: process.env.SLACK_APP_TOKEN
 }
 
 module.exports = {
