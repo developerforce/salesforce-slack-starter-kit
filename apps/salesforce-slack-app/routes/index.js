44f2b7180be193dfaac60d841e2de5efc61d388c
Alba Rivas <arivas@salesforce.com>
2022-02-22 12:45:05 +0100

    Remove unneeded route

--- apps/salesforce-slack-app/routes/index.js
+++ apps/salesforce-slack-app/routes/index.js
@@ -1,13 +1,11 @@
 'use strict';
 const { oauthStart } = require('./oauth-start');
 const { oauthCallback } = require('./oauth-callback');
-const { salesforceMessageCallback } = require('./salesforce-message-callback');
 
 const registerCustomRoutes = () => {
     const routes = [];
     routes.push(oauthStart);
     routes.push(oauthCallback);
-    routes.push(salesforceMessageCallback);
     return routes;
 };
 
